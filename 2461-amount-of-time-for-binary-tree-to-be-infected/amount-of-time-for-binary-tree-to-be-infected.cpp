b8dd2f8fdc19f94e1b9ce012a4a61e862f8b0edd
Anshaj07 <anshaj508@gmail.com>
2026-09-09 22:00:07 +0530

    Time: 171 ms (52.51%) | Memory: 194.4 MB (57.50%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -1,66 +1,81 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void MarkParent(TreeNode* root , unordered_map<TreeNode* , TreeNode*>& parent){
-        if(!root) return;
-        if(root->left){
-            parent[root->left]=root;
-            MarkParent(root->left , parent);
+
+    void MarkParent(TreeNode* root,
+                    unordered_map<TreeNode*, TreeNode*>& parent) {
+
+        if (!root) return;
+
+        if (root->left) {
+            parent[root->left] = root;
+            MarkParent(root->left, parent);
         }
-        if(root->right){
-            parent[root->right]=root;
-            MarkParent(root->right ,parent);
+
+        if (root->right) {
+            parent[root->right] = root;
+            MarkParent(root->right, parent);
         }
     }
-    TreeNode* FindStart(TreeNode* root,int start){
-        if(!root) return NULL;
-        if(root->val ==start){
+
+    TreeNode* FindStart(TreeNode* root, int start) {
+
+        if (!root) return NULL;
+
+        if (root->val == start)
             return root;
-        }
-        TreeNode* left = NULL;
-        left = FindStart(root->left , start);
-        if(left) return left;
-        return FindStart(root->right,start);
+
+        TreeNode* left = FindStart(root->left, start);
+
+        if (left) return left;
+
+        return FindStart(root->right, start);
     }
+
     int amountOfTime(TreeNode* root, int start) {
-        unordered_map<TreeNode* , TreeNode*> parent;
-        MarkParent(root,parent);
+
+        unordered_map<TreeNode*, TreeNode*> parent;
+        MarkParent(root, parent);
+
         unordered_map<TreeNode*, bool> visited;
-        TreeNode* Start = FindStart(root,start);
-        visited[Start] = true;
+
+        TreeNode* Start = FindStart(root, start);
+
         queue<TreeNode*> q;
         q.push(Start);
-        int ans=-1;
-        while(!q.empty()){
+        visited[Start] = true;
+
+        int ans = -1;
+
+        while (!q.empty()) {
+
             int size = q.size();
-            for(int i=0;i<size;i++){
+
+            for (int i = 0; i < size; i++) {
+
                 TreeNode* front = q.front();
                 q.pop();
-                if(front->left && !visited[front->left]){
-                    visited[front->left]=true;
+
+                if (front->left && !visited[front->left]) {
+                    visited[front->left] = true;
                     q.push(front->left);
                 }
-                if(front->right && !visited[front->right]){
-                    visited[front->right]=true;
+
+                if (front->right && !visited[front->right]) {
+                    visited[front->right] = true;
                     q.push(front->right);
                 }
-                if(parent[front] && !visited[parent[front]]){
-                    visited[parent[front]]= true;
+
+                if (parent.count(front) &&
+                    !visited[parent[front]]) {
+
+                    visited[parent[front]] = true;
                     q.push(parent[front]);
                 }
             }
+
             ans++;
         }
+
         return ans;
     }
 };
